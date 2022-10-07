d3e940dd78407d8c1dceb80e98da6b91278a7b39
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-10-07 14:05:12 +0700

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -2,12 +2,12 @@ let limit = 30
 let yts = require('yt-search')
 let fetch = require('node-fetch')
 const { servers, yta, ytv } = require('../lib/y2mate')
-let handler = async (m, { conn, command, text, isPrems, isOwner }) => {
-  if (!text) throw 'Cari apa?'
+let handler = async (m, { conn, command, usedPrefix, text, isPrems, isOwner }) => {
+  if (!text) throw `What song are you want find?\n*Example:* ${usedPrefix}play alan walker faded`
   let chat = global.db.data.chats[m.chat]
   let results = await yts(text)
   let vid = results.all.find(video => video.seconds < 3600)
-  if (!vid) throw 'Video/Audio Tidak ditemukan'
+  if (!vid) throw 'Video/Audio Not Found'
   let isVideo = /2$/.test(command)
   let yt = false
   let usedServer = servers[0]
@@ -18,17 +18,19 @@ let handler = async (m, { conn, command, text, isPrems, isOwner }) => {
       usedServer = server
       break
     } catch (e) {
-      m.reply(`Server ${server} error!${servers.length >= i + 1 ? '' : '\nmencoba server lain...'}`)
+      m.reply(`Server ${server} error!${servers.length >= i + 1 ? '' : '\ntry another server...'}`)
     }
   }
-  if (yt === false) throw 'Semua server tidak bisa :/'
+  if (yt === false) throw 'All servers can\'t 😕'
   let { dl_link, thumb, title, filesize, filesizeF } = yt
   let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
   conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
-*Source:* ${vid.url}
-*${isLimit ? 'Pakai ': ''}Link:* ${dl_link}
+*${title}*
+*⇄ㅤ     ◁   ㅤ  ❚❚ㅤ     ▷ㅤ     ↻*
+
+*📂ukuran video:* ${filesizeF}
+*👉🏻*Source:* ${vid.url}
+*${isLimit ? 'Pakai ': ''}Link:* ${await shortlink(dl_link)}
 *Server y2mate:* ${usedServer}
 `.trim(), m)
 let _thumb = {}
@@ -44,12 +46,18 @@ if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp' + (3 + /2$/.test(comm
   asDocument: chat.useDocument
 })
 }
-handler.help = ['play', 'play2'].map(v => v + ' <pencarian>')
+handler.help = ['play', 'play2'].map(v => v + ' <search>')
 handler.tags = ['downloader']
 handler.command = /^play2?$/i
+handler.premium = false
+handler.group = true
 
 handler.exp = 0
 handler.limit = true
 
 module.exports = handler
 
+async function shortlink(url) {
+isurl = /https?:\/\//.test(url)
+return isurl ? (await require('axios').get('https://tinyurl.com/api-create.php?url='+encodeURIComponent(url))).data : ''
+}
