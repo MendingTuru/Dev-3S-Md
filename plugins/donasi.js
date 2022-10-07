0de92545f7a33527cf53222e9a04dd04cf446725
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-10-07 14:12:30 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,9 +1,9 @@
 let handler = async m => conn.sendButton(m.chat, `
       ━━━━『 *DONASI KAK* 』━━━━
-➸ Dana : 0831-7588-6677
-➸ Gopay : 0831-7588-6677
-➸ Pulsa : 0831-7588-6677 
-➸ Saweria : https://saweria.co/3SBot
+➸ Dana : 0878-3214-7584
+➸ Gopay : 0878-3214-7584
+➸ Pulsa : 0878-3214-7584 
+➸ Saweria : https://saweria.co/kokopangeran
 *➸ QRIS : Chat Owner*
 
 (Melanggar Rules Di Ban 1 hari) 
